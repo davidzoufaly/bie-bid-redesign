3e50b0cb7f563dcb72c5370665d1c642359748a1
David Zoufalý <davidzoufaly@gmail.com>
2019-09-17 18:12:19 +0200

    idk update

--- pages/homepage.php
+++ pages/homepage.php
@@ -6,7 +6,11 @@ get_header();
 ?>
 <body>
     <!-- subheader -->
+<<<<<<< HEAD
     <div class="subheader-homepage">
+=======
+    <div id="subheader_homepage">
+>>>>>>> idk update
         <div class="container">
             <h1 class="subheader-homepage__title">BEST <span class="subheader-homepage__title--red subheader-homepage__title--light">IN</span> ENGLISH <span class="subheader-homepage__title--light">2019</span></h1>
             <div class="js-timer">
@@ -58,9 +62,15 @@ get_header();
         </div>
     </div>
     <!-- Boxes section -->
+<<<<<<< HEAD
     <div class="boxes-section">
         <div class="container">
             <div class="boxes-section__what-is">
+=======
+    <div id="boxes-section">
+        <div class="container">
+            <div class="_what-is">
+>>>>>>> idk update
                 <div class="--flex-lg-helper">
                     <h2>What is Best in English contest?</h2>
                     <p>The Best In English contest is a truly European online English language competition, open to all secondary schools within the EU and beyond. The test is designed for high school students aged between 15 and 19.</p>
@@ -68,6 +78,7 @@ get_header();
                 </div>
                 <div class="--flex-lg-video-helper">
                     <div class="_video">
+<<<<<<< HEAD
                         <div class="video-button"></div>
                     </div>
                 </div>
@@ -93,10 +104,38 @@ get_header();
                     </div>
                 </div>
                 <div class="boxes-section__aktuality__vsechny">
+=======
+                        <span class="icon-button_blue"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span>
+                    </div>
+                </div>
+            </div>
+            <div class="_aktuality">
+                <h3>AKTUALITY</h3>
+                <div class="--grid-lg-helper">
+                    <div class="_prispevek">
+                        <div class="_cover">
+                            <img  class="_cover-img" src="https://via.placeholder.com/240" alt="">
+                        </div>
+                        <h4>Tittle novinky na dva řádky lorem</h4>
+                        <p class="_date">22. 7. 2019</p>
+                        <p class="_text">Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem...</p>
+                    </div>
+                    <div class="_prispevek">
+                        <div class="_cover">
+                            <img  class="_cover-img" src="https://via.placeholder.com/240" alt="">
+                        </div>
+                        <h4>Tittle novinky na dva řádky lorem</h4>
+                        <p class="_date">22. 7. 2019</p>
+                        <p class="_text">Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem...</p>
+                    </div>
+                </div>
+                <div class="_vsechny">
+>>>>>>> idk update
                     <a class="btn btn--blue" href="#">VŠECHNY AKTUALITY</a>
                 </div>
             </div>
 
+<<<<<<< HEAD
             <div class="boxes-section__pro-media">
                 <h3>PRO MÉDIA</h3>
                 <div class="prispevek">
@@ -106,10 +145,22 @@ get_header();
                     <h4>Tittle média na dva řádky lorem</h4>
                     <p class="prispevek__text">Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Pellentesque pretium lectus id turpis. Donec ipsum massa. Sed vel lectus. Donec odio tempus molestie.</p>
                     <div><a class="btn btn--link" href="#">Více informací <span class="icon-arrow_link"></span></a></div>
+=======
+            <div class="_pro-media">
+                <h3>PRO MÉDIA</h3>
+                <div class="_prispevek">
+                    <div class="_cover">
+                        <img class="_cover-img" src="https://via.placeholder.com/240" alt="">
+                    </div>
+                    <h4>Tittle média na dva řádky lorem</h4>
+                    <p class="_text">Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Pellentesque pretium lectus id turpis. Donec ipsum massa. Sed vel lectus. Donec odio tempus molestie.</p>
+                    <div><a class="_vice-info" href="#">Více informací</a><span class="icon-arrow_link"></span></div>
+>>>>>>> idk update
                 </div>
             </div>
             <div class="boxes-section__pro-partnery">
                 <h3>PRO PARTNERY</h3>
+<<<<<<< HEAD
                 <div class="prispevek">
                     <div class="prispevek__cover">
                         <img class="prispevek__img" src="https://via.placeholder.com/240" alt="">
@@ -117,6 +168,15 @@ get_header();
                     <h4>Tittle média na dva řádky lorem</h4>
                     <p class="prispevek__text">Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Pellentesque pretium lectus id turpis. Donec ipsum massa. Sed vel lectus. Donec odio tempus molestie.</p>
                     <div><a class="btn btn--link" href="#">Více informací <span class="icon-arrow_link"></span></a></div>
+=======
+                <div class="_prispevek">
+                    <div class="_cover">
+                        <img class="_cover-img" src="https://via.placeholder.com/240" alt="">
+                    </div>
+                    <h4>Tittle média na dva řádky lorem</h4>
+                    <p class="_text">Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Pellentesque pretium lectus id turpis. Donec ipsum massa. Sed vel lectus. Donec odio tempus molestie.</p>
+                    <div><a class="_vice-info" href="#">Více informací</a><span class="icon-arrow_link"></span></div>
+>>>>>>> idk update
                 </div>
             </div>
         </div>
@@ -126,6 +186,7 @@ get_header();
         <div class="container">
             <h2>Reference</h2>
             <div class="_grid-reference">
+<<<<<<< HEAD
                 <div class="ref-box">
                     <div class="ref-box__avatar">
                         <img src="https://via.placeholder.com/100" alt="">
@@ -152,6 +213,34 @@ get_header();
                     <div class="ref-box__school">Liceo Scientifico Enrico Fermi</div>
                     <div class="ref-box__country">Padova, Italy</div>
                     <p class="ref-box__ref-text">Thank you to all the staff of BIE. Great organization and great fun for the students who enjoyed watching photos of their peers from far - and at the same time near - foreign countries</p>
+=======
+                <div class="_box">
+                    <div class="_avatar">
+                        <img src="https://via.placeholder.com/100" alt="">
+                    </div>
+                    <p class="_name --bold">Martina Kir Jambrek</p>
+                    <p class="_school">Ekonomska škola</p>
+                    <p class="_country">Požega, Croatia</p>
+                    <p class="_ref-text">Thank you! I'm truly happy that our student and school were successful this year and I'm looking forward to ourfuture participation in the Contest.</p>
+                </div>
+                <div class="_box">
+                    <div class="_avatar">
+                        <img src="https://via.placeholder.com/100" alt="">
+                    </div>
+                    <p class="_name --bold">Loreta Vainauskienė</p>
+                    <p class="_school">Kruonis Gymnasium</p>
+                    <p class="_country">Kruonis, Lithuania</p>
+                    <p class="_ref-text">Dear Andrea, Thank your for your help, it was a pleasure keeping in touch with you.</p>
+                </div>
+                <div class="_box">
+                    <div class="_avatar">
+                        <img src="https://via.placeholder.com/100" alt="">
+                    </div>
+                    <p class="_name --bold">Alessandra Baciga</p>
+                    <p class="_school">Liceo Scientifico Enrico Fermi</p>
+                    <p class="_country">Padova, Italy</p>
+                    <p class="_ref-text">Thank you to all the staff of BIE. Great organization and great fun for the students who enjoyed watching photos of their peers from far - and at the same time near - foreign countries</p>
+>>>>>>> idk update
                 </div>
             </div>
             <div class="btn-wrap-center"><a class="btn btn--white">VŠECHNY REFERENCE</a></div>    
@@ -161,10 +250,17 @@ get_header();
     <div class="partneri-sekce">
         <div class="container">
             <h2>Partneři</h2>
+<<<<<<< HEAD
             <div class="partneri">
                 <div class="partner">
                     <h3>ORGANIZÁTOR</h3>
                     <div class="partner__logo">
+=======
+            <div class="_partneri-box">
+                <div class="_partner">
+                    <h3>ORGANIZÁTOR</h3>
+                    <div class="_partner-logo">
+>>>>>>> idk update
                         <img src="./wp-content/themes/bie/img/czech-us.jpg" alr="Czech-us logo">
                     </div>
                     <h4 class="partner__nazev">Czech-us</h4>
