3e68853c016db42624feb48a59a475165ebfe328
David Zoufalý <davidzoufaly@gmail.com>
2019-09-17 18:12:19 +0200

    idk update

--- pages/homepage.php
+++ pages/homepage.php
@@ -5,7 +5,8 @@ get_header();
 
 ?>
 <body>
-    <div id="subheader">
+    <!-- subheader -->
+    <div id="subheader_homepage">
         <div class="container">
             <h1 class="_title">BEST <span class="--red --light">IN</span> ENGLISH <span class="--light">2019</span></h1>
             <div class="_js-timer">
@@ -57,50 +58,66 @@ get_header();
             </div>
         </div>
     </div>
-
+    <!-- Boxes section -->
     <div id="boxes-section">
         <div class="container">
-            <div class="what_is">
-                <h2>What is Best in English contest?</h2>
-                <p>The Best In English contest is a truly European online English language competition, open to all secondary schools within the EU and beyond. The test is designed for high school students aged between 15 and 19.</p>
-                <p>Watch a short video to learn more about the Best In English contest.</p>
-                <div class="video"></div>
+            <div class="_what-is">
+                <div class="--flex-lg-helper">
+                    <h2>What is Best in English contest?</h2>
+                    <p>The Best In English contest is a truly European online English language competition, open to all secondary schools within the EU and beyond. The test is designed for high school students aged between 15 and 19.</p>
+                    <p class="--bold">Watch a short video to learn more about the Best In English contest.</p>
+                </div>
+                <div class="--flex-lg-video-helper">
+                    <div class="_video">
+                        <span class="icon-button_blue"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span>
+                    </div>
+                </div>
             </div>
-            <div class="aktuality">
+            <div class="_aktuality">
                 <h3>AKTUALITY</h3>
-                <div class="prispevek">
-                    <img  class="_cover" src="" alt="">
-                    <h4>Tittle novinky na dva řádky lorem</h4>
-                    <span class="_date">22. 7. 2019</span>
-                    <p class="_text">Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem...</p>
+                <div class="--grid-lg-helper">
+                    <div class="_prispevek">
+                        <div class="_cover">
+                            <img  class="_cover-img" src="https://via.placeholder.com/240" alt="">
+                        </div>
+                        <h4>Tittle novinky na dva řádky lorem</h4>
+                        <p class="_date">22. 7. 2019</p>
+                        <p class="_text">Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem...</p>
+                    </div>
+                    <div class="_prispevek">
+                        <div class="_cover">
+                            <img  class="_cover-img" src="https://via.placeholder.com/240" alt="">
+                        </div>
+                        <h4>Tittle novinky na dva řádky lorem</h4>
+                        <p class="_date">22. 7. 2019</p>
+                        <p class="_text">Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem...</p>
+                    </div>
                 </div>
-                <div class="prispevek">
-                    <img  class="_cover" src="" alt="">
-                    <h4>Tittle novinky na dva řádky lorem</h4>
-                    <span class="_date">22. 7. 2019</span>
-                    <p class="_text">Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem...</p>
-                </div>
-                <div class="vsechny">
+                <div class="_vsechny">
                     <a class="btn btn--blue" href="#">VŠECHNY AKTUALITY</a>
                 </div>
             </div>
 
-            <div class="pro-media">
+            <div class="_pro-media">
                 <h3>PRO MÉDIA</h3>
-                <div class="prispevek">
-                    <img class="_cover" src="" alt="">
+                <div class="_prispevek">
+                    <div class="_cover">
+                        <img class="_cover-img" src="https://via.placeholder.com/240" alt="">
+                    </div>
                     <h4>Tittle média na dva řádky lorem</h4>
                     <p class="_text">Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Pellentesque pretium lectus id turpis. Donec ipsum massa. Sed vel lectus. Donec odio tempus molestie.</p>
-                    <a class="_vice-info" href="#">Více informací<span class=""> arrow</a>
+                    <div><a class="_vice-info" href="#">Více informací</a><span class="icon-arrow_link"></span></div>
                 </div>
             </div>
             <div class="pro-partnery">
                 <h3>PRO PARTNERY</h3>
-                <div class="prispevek">
-                    <img class="_cover" src="" alt="">
+                <div class="_prispevek">
+                    <div class="_cover">
+                        <img class="_cover-img" src="https://via.placeholder.com/240" alt="">
+                    </div>
                     <h4>Tittle média na dva řádky lorem</h4>
                     <p class="_text">Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Pellentesque pretium lectus id turpis. Donec ipsum massa. Sed vel lectus. Donec odio tempus molestie.</p>
-                    <a class="_vice-info" href="#">Více informací<span class=""> arrow</a>
+                    <div><a class="_vice-info" href="#">Více informací</a><span class="icon-arrow_link"></span></div>
                 </div>
             </div>
         </div>
@@ -109,32 +126,32 @@ get_header();
     <div id="reference">
         <div class="container">
             <h2>Reference</h2>
-            <div class="_row">
-                <div class="box">
+            <div class="_grid-reference">
+                <div class="_box">
                     <div class="_avatar">
-                        <img src="" alt="">
+                        <img src="https://via.placeholder.com/100" alt="">
                     </div>
-                    <div class="_name">Martina Kir Jambrek</div>
-                    <div class="_school">Ekonomska škola</div>
-                    <div class="_country">Požega, Croatia</div>
+                    <p class="_name --bold">Martina Kir Jambrek</p>
+                    <p class="_school">Ekonomska škola</p>
+                    <p class="_country">Požega, Croatia</p>
                     <p class="_ref-text">Thank you! I'm truly happy that our student and school were successful this year and I'm looking forward to ourfuture participation in the Contest.</p>
                 </div>
-                <div class="box">
+                <div class="_box">
                     <div class="_avatar">
-                        <img src="" alt="">
+                        <img src="https://via.placeholder.com/100" alt="">
                     </div>
-                    <div class="_name">Loreta Vainauskienė</div>
-                    <div class="_school">Kruonis Gymnasium</div>
-                    <div class="_country">Kruonis, Lithuania</div>
+                    <p class="_name --bold">Loreta Vainauskienė</p>
+                    <p class="_school">Kruonis Gymnasium</p>
+                    <p class="_country">Kruonis, Lithuania</p>
                     <p class="_ref-text">Dear Andrea, Thank your for your help, it was a pleasure keeping in touch with you.</p>
                 </div>
-                <div class="box">
+                <div class="_box">
                     <div class="_avatar">
-                        <img src="" alt="">
+                        <img src="https://via.placeholder.com/100" alt="">
                     </div>
-                    <div class="_name">Alessandra Baciga</div>
-                    <div class="_school">Liceo Scientifico Enrico Fermi</div>
-                    <div class="_country">Padova, Italy</div>
+                    <p class="_name --bold">Alessandra Baciga</p>
+                    <p class="_school">Liceo Scientifico Enrico Fermi</p>
+                    <p class="_country">Padova, Italy</p>
                     <p class="_ref-text">Thank you to all the staff of BIE. Great organization and great fun for the students who enjoyed watching photos of their peers from far - and at the same time near - foreign countries</p>
                 </div>
             </div>
@@ -145,19 +162,19 @@ get_header();
     <div id="partneri">
         <div class="container">
             <h2>Partneři</h2>
-            <div class="_partneri-row">
+            <div class="_partneri-box">
                 <div class="_partner">
                     <h3>ORGANIZÁTOR</h3>
                     <div class="_partner-logo">
-                        <img src="" alr="">
+                        <img src="./wp-content/themes/bie/img/czech-us.jpg" alr="Czech-us logo">
                     </div>
                     <h4 class="_nazev">Czech-us</h4>
                     <p class="_about">Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem.</p>
                 </div>
                 <div class="_partner">
                     <h3>SPOLUORGANIZÁTOR</h3>
-                    <div class="_partner-logo">
-                        <img src="" alr="">
+                    <div class="_partner-logo" style="margin-bottom: 16px">
+                        <img src="./wp-content/themes/bie/img/institute.jpg" alr="Institute for competencies development logo">
                     </div>
                     <h4 class="_nazev">Czech-us</h4>
                     <p class="_about">Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem.</p>
@@ -165,70 +182,55 @@ get_header();
                 <div class="_partner">
                     <h3>GENERÁLNÍ PARTNER</h3>
                     <div class="_partner-logo">
-                        <img src="" alr="">
+                        <img src="./wp-content/themes/bie/img/kb.jpg" alr="Komercni banka logo">
                     </div>
-                    <h4 class="_nazev">Czech-us</h4>
+                    <h4 class="_nazev">Komerční banka</h4>
                     <p class="_about">Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem.</p>
                 </div>
-            </div>
-            <div class="_hl-a-med">
                 <div class="_hl-partneri">
                     <h4>HLAVNÍ PARTNEŘI</h4>
                     <div class="_loga">
-                        <img src="" alt="">
-                        <img src="" alt="">
-                        <img src="" alt="">
-                        <img src="" alt="">
+                        <img src="./wp-content/themes/bie/img/stabilo.jpg" alt="Stabilo logo">
+                        <img src="./wp-content/themes/bie/img/ilac.jpg" alt="ILAC logo">
+                        <img src="./wp-content/themes/bie/img/akcent.jpg" alt="Akcent logo">
+                        <img src="./wp-content/themes/bie/img/bhv.jpg" alt="BVH education logo">
+                        <img src="./wp-content/themes/bie/img/injoy.jpg" alt="Injoy logo">
                     </div>
                 </div>
                 <div class="_med-partneri">
                     <h4>MEDIALNÍ PARTNEŘI</h4>
                     <div class="_loga">
-                        <img src="" alt="">
-                        <img src="" alt="">
-                        <img src="" alt="">
-                        <img src="" alt="">
+                        <img src="./wp-content/themes/bie/img/ess.jpg" alt="European summer school logo">  
+                        <img src="./wp-content/themes/bie/img/brainstorm.jpg" alt="Brainstorm logo">
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="libi-se">
+    <div id="libi-se">
         <h2>Líbí se ti tato soutěž?</h2>
         <a class="btn btn--white" href="#">REGISTRUJ SE</a>
-        <a class="btn btn--link"></a>
+        <a class="btn btn--link">STAŇ SE PARTNEREM</a>
     </div>
 
-    <div class="kontaktujte-nas">
-        <h3>KONTAKTUJTE NÁS</h3>
-        <div class="formular">
-            <form action="" method="post">
-                <input class="" type="text" name="jmeno" placeholder="Jméno*">
-                <input class="" type="email" name="email" placeholder="Email*">
-                <input type="text" name="text" placeholder="Text*">
-                <input class="btn btn--blue" type="submit" name="odeslat" value="Odeslat">
-            </form>
-        </div>
-    </div>
-
-    <div class="footer">
-        <div class="_footer-menu">
-            <a class="_footer-menu-item" href="#">Kontakt</a>
-            <a class="_footer-menu-item" href="#">Archiv novinek</a>
-            <a class="_footer-menu-item" href="#">Cookies</a>
-            <a class="_footer-menu-item" href="#">OOÚ</a>
-        </div>
-        <div class="_footer-logo">
-            <img src="" alt="">
-            <img src="" alt="">
-        </div>
-        <div class="copy">
-            <p>© 2003-2019 Copyright Czech-us v.o.s.</p>
-            <p>Travel service and agencyID no.: 27456927</p>
+    <div id="kontaktujte-nas">
+        <div class="container">
+            <h3>KONTAKTUJTE NÁS</h3>
+            <div class="formular">
+                <form action="" method="post">
+                    <input class="_jmeno" type="text" name="jmeno" placeholder="Jméno*">
+                    <input class="_email" type="email" name="email" placeholder="Email*">
+                    <input class="_text" type="text" name="text" placeholder="Text*">
+                    <input class="btn btn--blue" type="submit" name="odeslat" value="ODESLAT">
+                </form>
+            </div>
         </div>
     </div>
 
+    <?php 
+        get_footer();
+    ?>
 
     <script>
 
@@ -261,5 +263,4 @@ get_header();
             }
         }, 1000);
     </script>
-</body>
-</html>
+
