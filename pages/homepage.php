025a3ab13f04ed6d244b3d1df8e1734623eaad69
David Zoufalý <davidzoufaly@gmail.com>
2019-09-18 22:41:00 +0200

    po mergi s jakubem - vse vypada ok

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,11 +1,9 @@
 <?php /* Template Name: homepage */ ?>
 <?php get_header(); ?>
-
 <?php get_template_part('parts/subheader-homepage'); ?>
 <?php get_template_part('parts/boxes-section-homepage') ?>
 <?php get_template_part('modules/module-reference') ?>
 <?php get_template_part('parts/partneri-homepage') ?>
 <?php get_template_part('modules/module-libi-se') ?>
 <?php get_template_part('modules/module-kontakt') ?>
-
 <?php get_footer(); ?>
