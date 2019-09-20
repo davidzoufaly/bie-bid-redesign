ab490592b0b081821cb386f77be0cc2676ac30a3
jacob_LTS <jakub.vozenilek.41@gmail.com>
2019-09-20 12:09:18 +0200

    menu update

--- js/footer/test-footer.js
+++ js/footer/test-footer.js
@@ -14,6 +14,10 @@ const menuImg = document.getElementsByClassName('logo-img')[0];
 const menuItem = document.querySelectorAll('.menu-item a');
 const subM = document.getElementsByClassName('arrow__up');
 
+/* bile menu na telefonu */ 
+if(window.innerWidth < 991) {
+    whiteNav();
+}
 
 
 
@@ -23,13 +27,13 @@ const subM = document.getElementsByClassName('arrow__up');
 
 /* pri scrollu vola FCI scrollNav() */
 window.addEventListener('scroll', (e) => {
-    scrollNav(e.path[1].scrollY);
+    scrollNav(e.path[1].pageYOffset);
 })
 
 /* kdyz se klikne na 'menu-item-has-children' ukaz submenu */
+/* submenu listener, pokud ma deti je z nej schovany dropdown */
 const submenu = document.querySelectorAll('.sub-menu');
 window.addEventListener('click', (e) => {
-    /* submenu listener, pokud ma deti je z nej schovany dropdown */
     if(e.path[1].classList.contains('menu-item-has-children')) {
         dropDown(e);
     } else {
@@ -44,7 +48,7 @@ menu.addEventListener('mouseover', () => {
 
 /* kdyz kurzor odejde z menu dej ho zpet na transparent pokud je uzivatel uplne na hore a submeny schovej */
 menu.addEventListener('mouseleave', () => {
-    if(window.scrollY === 0) {
+    if(window.pageYOffset === 0) {
         transNav();
     }
     resetSubmenu(true);
@@ -56,9 +60,12 @@ menu.addEventListener('mouseleave', () => {
 \****************************/
 
 /* FCE pokud je uzivatel nahore bude menu trasparentni, pokud jinde bude bile*/
-function scrollNav(scrollY) {
-    if(scrollY === 0) {
-        transNav();
+function scrollNav(position) {
+    if(position === 0) {
+        if(window.innerWidth >= 991) {
+            transNav();
+        }
+        
     } else {
         whiteNav();
     }
@@ -128,38 +135,38 @@ function dropDownArrows() {
 
 /* FCE vysouva dropdowny  */
 function dropDown(event) {
-    
+        
     event.target.classList.toggle('sub-menu--revealed');
+    event.target.children[0].classList.toggle('arrow__up--animate');
     resetSubmenu(false);
     event.target.children[0].classList.toggle('arrow__up--blue');
-    
+
     
     let target = event.path[1].children[1];
 
-    if(window.screen.width >= 991) {
         if(target.style.maxHeight) {
             target.style.maxHeight = null;
+            
         } else {
             target.style.maxHeight = target.scrollHeight + 'px';
+            
         }
-
-    } else {
-        if(target.style.maxHeight) {
-            target.style.maxHeight = null;
-        } else {
-            target.style.maxHeight = target.scrollHeight + 'px';
-        }
-    }
 }
 
 /* FCE resetuje submeny zpatky */
 function resetSubmenu(kombo) {
+    // if (typeof optionalParam === 'undefined') {
+    //     optionalParam = 'default'; 
+    // } else {
+    //     optionalParam.classList.toggle('sub-menu--revealed');
+    // }
     let subMenus = document.getElementsByClassName('sub-menu');
 
     if(kombo) {
         for(let j = 0; j<subM.length; j++) {
             if(subM[j].classList.contains('arrow__up--blue')) {
-                subM[j].classList.remove('arrow__up--blue');
+                subM[j].classList.remove('arrow__up--blue', 'arrow__up--animate');
+
             }
         }
     }
@@ -172,5 +179,7 @@ function resetSubmenu(kombo) {
             el.classList.remove('sub-menu--revealed'); 
         }
     }
+
+    // return optionalParam;
 }
 
