5db8d639f6af4f44f709ae9f935e8aa857ae803c
David Zoufalý <davidzoufaly@gmail.com>
2019-09-26 12:18:03 +0200

    Reg updaate

--- header.php
+++ header.php
@@ -5,8 +5,8 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
-    <link href="//www.google-analytics.com" rel="dns-prefetch">
-    <link href="//www.czech-us.cz" rel="dns-prefetch">
+    <link href="https://www.google-analytics.com" rel="preconnect">
+    <link href="https://www.czech-us.cz" rel="preconnect">
 
     <link rel="apple-touch-icon" sizes="57x57"
         href="<?php echo get_template_directory_uri() ?>/img/icons/apple-icon-57x57.png">
