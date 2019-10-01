dd94bbe8ef883b5ab45b5c94c10a90f47f2feb99
David Zoufalý <davidzoufaly@gmail.com>
2019-10-01 12:43:06 +0200

    generating id h2-h4 updated

--- layout/layout-heading-three.php
+++ layout/layout-heading-three.php
@@ -4,6 +4,8 @@ if(get_sub_field('styl_3') === true) :
 else : 
     $class_list = "h3--nobefore";
 endif;
+
+$h3 = get_sub_field('nadpis_3_urovne');
 ?> 
 
-<h3 class="<?php echo $class_list ?>"><?php the_sub_field('nadpis_3_urovne')?></h3>
+<h3 class="<?php echo $class_list ?>" id="<?php echo change_to_id($h3) ?>"><?php echo $h3 ?></h3>
