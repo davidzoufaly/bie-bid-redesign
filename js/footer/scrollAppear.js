c04270f8a841602a96d3b59de050a3035768e1e7
jacob_LTS <jakub.vozenilek.41@gmail.com>
2019-11-01 23:36:38 +0100

    scrollAppear.js isInViewport pridano

--- js/footer/scrollAppear.js
+++ js/footer/scrollAppear.js
@@ -1,4 +1,4 @@
-/* Styly najdes v abstracts/animateScroll.scss. Do html se elementu se prida class 'animateScroll animateScroll--arrival-up' */
+/* Styly najdes v abstracts/animateScroll.scss. Do html se elementu se pridat class 'animateScroll animateScroll--arrival-up' */
 /* Pak jen do js: 
     let scrollEvent = new ScrollAppear('animateScroll--arrival-up');
     scrollEvent.scrollListener();
@@ -24,12 +24,29 @@ class ScrollAppear {
       }
     });
   }
+
+  isInViewport() {
+    window.addEventListener("DOMContentLoaded", () => {
+      let elementsPos = [];
+      for (let i = 0; i < this.elements.length; i++) {
+        elementsPos.push(this.elements[i].getBoundingClientRect().top);
+        if (elementsPos[i] < window.innerHeight) {
+          this.elements[i].classList.add("animateScroll--animate");
+        } else {
+          this.elements[i].classList.remove("animateScroll--animate");
+        }
+      }
+    }) 
+  }
+
 }
 
 let scrollEvent = new ScrollAppear("animateScroll--arrival-up");
+scrollEvent.isInViewport();
 scrollEvent.scrollListener();
 
 window.addEventListener("resize", () => {
   let scrollEvent = new ScrollAppear("animateScroll--arrival-up");
+  scrollEvent.isInViewport();
   scrollEvent.scrollListener();
 });
