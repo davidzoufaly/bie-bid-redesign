716bafae086730a9b406184515836d1068d57465
David Zoufalý <davidzoufaly@gmail.com>
2019-11-30 14:26:16 +0100

    builder pismenkovy ol

--- layout/layout-list.php
+++ layout/layout-list.php
@@ -1,11 +1,25 @@
 <?php
 $typ = get_sub_field('typ_seznamu');
 
+$class = "";
+
+switch($typ) {
+    case "bod":
+        $typ_tag = "ul";
+    break;
+    case "cis":
+        $typ_tag = "ol";
+    break;
+    case "pis":
+        $typ_tag = "ol";
+        $class = "lower-alpha";
+}
+
 if(have_rows('seznam')) :
-    echo "<".$typ.">";
+    echo "<".$typ_tag." class=". $class .">";
     while(have_rows('seznam')) : the_row();
         echo "<li class='layout__list'><div>".get_sub_field('pole_seznamu')."</div></li>";
     endwhile;
-    echo "</".$typ.">";
+    echo "</".$typ_tag.">";
 endif;
 ?>
--- query-items/partner-secondary-partners.php
+++ query-items/partner-secondary-partners.php
@@ -5,5 +5,7 @@
         alt="<?php echo get_field('logo_partner')['alt']; ?>"
     />
     <h4 class="partner__nazev"><?php the_title() ?></h4>
-    <p class="partner__about"><?php echo get_field('text_partner'); ?></p>
+    <?php if(!empty(get_field('text_partner'))) : ?>
+        <p class="partner__about"><?php echo get_field('text_partner'); ?></p>
+    <?php endif; ?>
 </div>
