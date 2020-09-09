22636596b94829b365c45c8612bcf46bfe1c575c
jacob_LTS <jakub.vozenilek.41@gmail.com>
2020-09-09 12:16:58 +0200

    Only invoice payment method v3

--- js/scripts.payment.js
+++ js/scripts.payment.js
@@ -40,7 +40,7 @@ async function saveData() {
   ) {
     document.querySelector(".invoice-form__fieldset--payment").style.display =
       "block";
-    document.querySelector('.invoice-form__payment-method').style.display =
+    document.querySelector('.invoice-form__pay-now').style.display =
       "none";
     document.querySelector('.invoice-form__generate-invoice').style.display = 
       "block";  
